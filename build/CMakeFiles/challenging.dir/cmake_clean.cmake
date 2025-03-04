3f11f5e2f37965f84130092085ae72a1cd1a6b5b
mironpomazkov <mpomazkov@icloud.com>
2025-03-04 15:12:44 +0300

    Add biuld folder to challndge

---
+++ build/CMakeFiles/challenging.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/challenging.dir/challengind.cpp.o"
+  "CMakeFiles/challenging.dir/challengind.cpp.o.d"
+  "challenging"
+  "challenging.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/challenging.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
